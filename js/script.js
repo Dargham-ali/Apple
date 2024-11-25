2e99e06cda9d7648c9003cedac1c6b036c6fe39f
Dargham <160471978+Dargham-ali@users.noreply.github.com>
2024-11-25 11:53:35 +0300

    da

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let myimg = document.querySelector('.ceo')
+console.log(myimg);
+function chang(sor) {
+    myimg.setAttribute('src', sor);
+}
+function color(co) {
+    document.body.style.backgroundColor = co
+}
